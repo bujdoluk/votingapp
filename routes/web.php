0119a99c4dc5dfa8aa654b0915bd9a40c7e40bac
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2021-05-21 21:20:19 +0200

    second

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [\App\Http\Controllers\IdeaController::class, 'index'])->name('idea.index');
+Route::get('/ideas/{idea:slug}', [\App\Http\Controllers\IdeaController::class, 'show'])->name('idea.show');
 
 
 require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,6 +41,13 @@ module.exports = {
             maxWidth: {
                 custom: '68.5rem',
             },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
+            },
+            fontSize: {
+                xxs: ['0.625rem', { lineHeight: '1rem' }],
+            },
         },
     },
 
@@ -50,5 +57,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
