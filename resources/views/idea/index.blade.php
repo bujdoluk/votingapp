088a6e5fa2f074c8710c3f61c660540c1ff4eab0
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2021-05-22 07:42:03 +0200

    third

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -30,7 +30,19 @@
 
     <div class="ideas-container space-y-6 my-8">
         @foreach($ideas as $idea)
-            <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div
+                x-data
+                @click="
+                    const clicked = $event.target
+                    const target = clicked.tagName.toLowerCase()
+                    const ignores = ['button', 'svg', 'path', 'a']
+
+                    if(!ignores.includes(target)){
+                        clicked.closest('.idea-container').querySelector('.idea-link').click()
+                    }
+                "
+                class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer"
+            >
                 <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                     <div class="text-center">
                         <div class="font-semibold text-2xl">12</div>
@@ -44,12 +56,12 @@
                 <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
                     <div class="flex-none mx-2 md:mx-0">
                         <a href="#">
-                            <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                            <img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
                         </a>
                     </div>
                     <div class="w-full flex flex-col justify-between mx-2 md:mx-4">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="{{ route('idea.show', $idea) }}" class="hover:underline">{{ $idea->title }}</a>
+                            <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">{{ $idea->title }}</a>
                         </h4>
                         <div class="text-gray-600 mt-3 line-clamp-3">
                             {{ $idea->description }}
@@ -59,7 +71,7 @@
                             <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                                 <div>{{ $idea->created_at->diffForHumans() }}</div>
                                 <div>&bull;</div>
-                                <div>Category 1</div>
+                                <div>{{$idea->category->name}}</div>
                                 <div>&bull;</div>
                                 <div class="text-gray-900">3 Comments</div>
                             </div>
